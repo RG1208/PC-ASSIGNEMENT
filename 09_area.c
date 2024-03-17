e6592aae0719a95c90dc579251576f4abf340c8c
rachit <rachitgarg1208@gmail.com>
2024-03-17 12:40:33 +0530

    Pc Assignment

--- 09_area.c
+++ 09_area.c
@@ -1,92 +1,58 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    int shape;
-    float area;
+    int choice;
+    float length, width, base, height, radius, SlantHeight, area;
 
-    // square
-    float square_side;
-
-    // circle
-    float circle_radius;
-
-    // rectangle
-    float rect_length, rect_width;
-
-    // cone
-    float cone_radius, cone_slant_height;
-
-    // cylinder
-    float cylinder_radius, cylinder_height;
-
-    printf("\n");
+    printf("Enter the shape to calculate area:\n");
     printf("1. Square\n");
     printf("2. Circle\n");
     printf("3. Rectangle\n");
     printf("4. Cone\n");
-    printf("5. Cylinder\n");
+    printf("5. Cyclinder\n");
+    scanf("%d", &choice);
 
-    printf("Enter the value of n: ", shape);
-    scanf("%d", &shape);
-
-    switch (shape)
+    switch (choice)
     {
     case 1:
-
-        printf("Enter the side of the square: ");
-        scanf("%f", &square_side);
-
-        area = pow(square_side, 2);
-
+        printf("Enter the length of square:");
+        scanf("%f", &length);
+        area = length * length;
+        printf("Area of  Square: %f", area);
         break;
 
     case 2:
-        printf("Enter the radius of the circle: ");
-        scanf("%f", &circle_radius);
-
-        area = 3.14 * pow(circle_radius, 2);
-
+        printf("Enter radius of circle: ");
+        scanf("%f", &radius);
+        area = 3.14 * radius * radius;
+        printf("Area of circle = %f\n", area);
         break;
+
     case 3:
-        printf("Enter the length of the rectangle: ");
-        scanf("%f", &rect_length);
-
-        printf("Enter the width of the rectangle: ");
-        scanf("%f", &rect_width);
-
-        area = rect_length * rect_width;
-
+        printf("Enter length and width of Rectangle: ");
+        scanf("%f %f", &length, &width);
+        area = length * width;
+        printf("Area of  Square: %f", area);
         break;
+
     case 4:
-        printf("Enter the radius of the cone: ");
-        scanf("%f", &cone_radius);
-
-        printf("Enter the slant height of the cone: ");
-        scanf("%f", &cone_slant_height);
-
-        area = 3.14 * cone_radius * cone_slant_height + 3.14 * pow(cone_radius, 2);
-
+        printf("Enter radius and Slant Height of cone: ");
+        scanf("%f %f", &radius, &SlantHeight);
+        area = 3.14 * radius * (radius + SlantHeight);
+        printf("Area of Cone: %f", area);
         break;
+
     case 5:
-        printf("Enter the radius of the cylinder: ");
-        scanf("%f", &cylinder_radius);
-
-        printf("Enter the height of the cylinder: ");
-        scanf("%f", &cylinder_height);
-
-        area = 2 * (3.14 * pow(cylinder_radius, 2)) + 3.14 * cylinder_radius * cylinder_height;
-
+        printf("Enter radius and height of cyclinder: ");
+        scanf("%f %f", &radius, &height);
+        area = 2 * 3.14 * radius * (radius + height);
+        printf("Area of Cone: %f", area);
         break;
 
     default:
-        printf("Invalid Input\n");
-
-        break;
+        printf("Invalid Choice\n");
     }
-
-    printf("Area: %f\n", area);
 
     return 0;
 }
