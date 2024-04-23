a9230ae9dd20bf3337ac81774fb645d76b8d961a
rachit <rachitgarg1208@gmail.com>
2024-04-23 22:02:13 +0530

    Pc Assignment

--- 06_EvenOdd.c
+++ 06_EvenOdd.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    (num % 2 == 0) ? printf("Even") : printf("Odd");
+    (num % 2 == 0) ? printf("Even \n") : printf("Odd \n");
 
     return 0;
 }
--- 12_PrimeNumber.c
+++ 12_PrimeNumber.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
+// Function to check if a number is prime
+int isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return 0; // 0 and 1 are not prime
+    }
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return 0; // If the number is divisible by any number other than 1 and itself, it's not prime
+        }
+    }
+    return 1; // If the number is not divisible by any other number, it's prime
+}
+
 int main()
 {
     int num;
 
+    // Input
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if (num % 2 == 0 && 0)
+    // Check if the number is prime
+    if (isPrime(num))
     {
         printf("%d is a prime number.\n", num);
     }
